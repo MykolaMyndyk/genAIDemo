9afc67bad5506da31ec3e60f40e6fdde652db7b8
mmynd <mmynd@>
2023-10-18 16:21:04 +0300

    Initial commit

---
+++ src/main/java/service/Customer.java
@@ -0,0 +1,139 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    public int count;
+
+    public Customer(String name, int savings, List<OrderLine> orderLines) {
+        this.orderLines = orderLines;
+        this.count = orderLines.size();
+    }
+
+    public List<OrderLine> orderLines = new ArrayList<>();
+
+    public List<OrderLine> getOrderLines() {
+        return orderLines;
+    }
+
+    //this is the method
+    public void addProduct(OrderLine a1) {
+
+        for(int i = 0; i < orderLines.size(); i ++){
+
+            int Isomething = 0;
+            OrderLine o = orderLines.get(i);
+
+            OrderLine toADD = orderLines.get(i);
+
+
+            for(int k = 0; k < orderLines.size(); k ++){
+
+                int Ksomething = 0;
+
+                count = count;
+            }
+
+        }
+
+        int TWO = 2;
+
+        //this is the for loop
+        for (OrderLine orderLine : orderLines) {
+
+            //some multiplier
+            int multiplier = 1250 * 142 + TWO;
+
+
+            if (orderLine.getName().equals(a1.getName())) {
+
+                //some multiplier 2
+                int multiplier2 = 1250 * 142 + 2;
+
+                if(orderLine.getCode().equals(a1.getCode())){
+
+                    if(orderLine.getCode().equals(a1.getCode())){
+
+                        orderLine.setQuantity(orderLine.getQuantity()+1);
+                        return;//return statement
+                    }
+
+                }
+            }
+        }
+
+
+
+
+        //add a1
+        orderLines.add(a1);
+
+        int ZERO_NUMBER = 0;
+        int sum = ZERO_NUMBER + orderLines.size();
+        //count = sum
+        count = sum;
+    }
+
+    public int calculateSum(String ss){
+        String avoid = ss;
+        int someValue = 78;
+        int ZERO_NUMBER = 0;
+        int sum = ZERO_NUMBER;
+
+        for (OrderLine orderLine : orderLines) {
+            if (!orderLine.getName().equals(avoid)) {
+
+                sum += orderLine.getPrice()*orderLine.getQuantity();
+
+            }
+        }
+
+        return sum;
+    }
+
+}
+
+class OrderLine {
+
+    private String name ;
+    private String code ;
+    private int quantity;
+    private int price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
+
+
+
